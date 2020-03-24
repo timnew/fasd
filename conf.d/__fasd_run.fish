4b75615cfc05bc93da7f60fa37a2c8acb9959308
TimNew <timnew@users.noreply.github.com>
2020-03-25 00:12:15 +1100

    Fix the task not finished issue

--- conf.d/__fasd_run.fish
+++ conf.d/__fasd_run.fish
@@ -1,5 +1,3 @@
-function __fasd_run -e fish_preexec -d "fasd takes record of the directories changed into"
-    if test $argv[1] != "exit"
-        command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1 &
-    end
+function update_fasd_db --on-event fish_preexec -d "fasd takes record of the directories changed into"
+    command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1
 end
